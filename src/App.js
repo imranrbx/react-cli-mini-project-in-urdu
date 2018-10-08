9585367417b311833690ed268f83784e064901e5
Imran Qasim <imran_qasim@hotmail.com>
2018-10-08 06:14:16 +0500

    Part 11 React Cli Crash Course in Urdu

--- src/App.js
+++ src/App.js
@@ -1,64 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Addform from './components/Addform';
-import Task from './components/Task';
+import React, {Component} from 'react';
+import Navbar from './components/Navbar'
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      tasks:[]
-    }
-    this.addRecord = this.addRecord.bind(this);
-    this.delRecord = this.delRecord.bind(this);
-    this.getRecord = this.getRecord.bind(this);
-  }
-  componentDidMount() {
-    fetch('http://localhost:4000/tasks')
-    .then(res => res.json())
-    .then(tasks => this.setState({tasks}))
-    .catch(err => console.log(err));
-  }
-  getRecord(e){
-    const tasks = this.state.tasks.map(task => {
-      if(e.id == task.id){
-        return e
-      }
-      return task;
-    })
-    this.setState({tasks});
-  }
-  addRecord(e){
-    const tasks = [...this.state.tasks, e];
-    this.setState({tasks})
-  }
-  delRecord(e){
-   this.setState({tasks: e})
-  }
+
   render() {
-    const tasks = this.state.tasks.map(task => <Task key={task.id} {...task} onDel={this.delRecord} onUpdate={this.getRecord} />)
-    return (
-      <div className="App mt5">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro">
-      
-        <div className="container ">
-          <div className="row">
-            <div className="col-md-6 offset-md-3">
-            <Addform addRec={this.addRecord} />
-            <ul className="list-group">
-              <li className="list-group-item active">Todo Tasks List</li>
-              {tasks}
-            </ul>
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
-    );
+    return (<Navbar/>);
   }
 }
 
--- src/components/Addform.js
+++ src/components/Addform.js
@@ -1,33 +1,40 @@
 import React, {Component} from 'react';
 
-class Addform extends Component{
-    constructor(){
+class Addform extends Component {
+    constructor() {
         super();
-        this.addRecord = this.addRecord.bind(this);
+        this.addRecord = this
+            .addRecord
+            .bind(this);
     }
-    addRecord(e){
+    addRecord(e) {
         e.preventDefault();
-        fetch('http://localhost:4000/tasks',{
+        fetch('http://localhost:4000/tasks', {
             method: "POST",
-            headers:{
+            headers: {
                 'Content-Type': 'Application/json'
             },
             body: JSON.stringify({
                 "id": Math.floor((Math.random() + 1) * 0x100000),
-                "title": e.target.task.value,
-                "isCompleted": "false"
+                    "title": e.target.task.value,
+                    "isCompleted": "false"
+                })
             })
-        })
-        .then(res => res.json())
-        .then(result => this.props.addRec(result))
-        .catch(err => console.log(err));
+            .then(res => res.json())
+            .then(result => this.props.addRec(result))
+            .catch(err => console.log(err));
 
     }
-    render(){
-        return(
+    render() {
+        return (
             <form onSubmit={this.addRecord} className="mt5">
                 <div className="form-group">
-                    <input type="text" name="task" id="task" placeholder="Add new Task here..." className="form-control"/>
+                    <input
+                        type="text"
+                        name="task"
+                        id="task"
+                        placeholder="Add new Task here..."
+                        className="form-control"/>
                 </div>
                 <div className="form-group">
                     <input type="submit" value="Add task" className="btn btn-primary btn-block"/>
